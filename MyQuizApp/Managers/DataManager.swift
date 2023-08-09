c81c6ac2c1bea9182a1f96df007436201fce8d5a
YaNeGleb <zabrodagleb77@gmail.com>
2023-08-09 17:08:15 +0300

    1.Renamed json 2. Added 3 new question categories.

--- MyQuizApp/Managers/DataManager.swift
+++ MyQuizApp/Managers/DataManager.swift
@@ -11,7 +11,7 @@ struct DataManager {
     static func loadCategoriesFromJSON() -> [Category] {
         var categories: [Category] = []
         
-        if let url = Bundle.main.url(forResource: "jsonTest1", withExtension: "json"),
+        if let url = Bundle.main.url(forResource: "Questions", withExtension: "json"),
            let data = try? Data(contentsOf: url) {
             do {
                 let quizData = try JSONDecoder().decode(QuizData.self, from: data)
